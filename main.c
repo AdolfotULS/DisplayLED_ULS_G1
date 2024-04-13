0d85239dc4a03837f270797e7f7289c43d5400a6
AdolfotULS <166766031+AdolfotULS@users.noreply.github.com>
2024-04-13 18:51:52 -0400

    Mejora Controlador
    - Modificacion de Metodos del Controlador
    
    Co-Authored-By: NotSolaire <166455578+NotSolaire@users.noreply.github.com>
    Co-Authored-By: JereR1621 <164182331+JereR1621@users.noreply.github.com>

--- main.c
+++ main.c
@@ -6,44 +6,29 @@
 
 // --------------- GLOBALES ---------------
 
+#define TAMANO 8 // Tamaño matriz de LEDs
+
+const int pines_positivos[TAMANO] = {26, 19, 13, 6, 5, 11, 9, 10}; // Pines GPIO columnas
+
+const int pines_negativos[TAMANO] = {21, 20, 16, 12, 7, 8, 25, 24}; // Pines GPIO filas
+
 volatile sig_atomic_t senal_recibida = 0; // Almacenar la senal recibida
 
 // --------------- DECLARACIONES ---------------
 
-#define TAMANO 8 // Tamaño matriz de LEDs
-// Pines GPIO columnas
-const int pines_positivos[TAMANO] = {26, 19, 13, 6, 5, 11, 9, 10};
-// Pines GPIO filas
-const int pines_negativos[TAMANO] = {21, 20, 16, 12, 7, 8, 25, 24};
+
 
 // --------------- MAIN ---------------
-int main() {
-    
-    // Inicializa la biblioteca pigpio
-    if (gpioInitialise() == PI_INIT_FAILED) {
-        printf("ERROR: No se pudo inicializar GPIO.\n");
+
+void main() {
+    signal(SIGINT, sigint_handler);
+    if (inicializar_gpio == 0){
+        printf("No se ha podido Inicializar GPIO.\n")
         return 1;
     }
 
-    // Inicializa los LEDs
-    inicializar_leds(pines_positivos, pines_negativos);
+    //Menu
 
-    // Registra la función sigint_handler para manejar la senal de interrupcion
-    signal(SIGINT, sigint_handler);
-    printf("Presionar CTRL-C para salir.\n");
-
-    // Prueba de los LEDs hasta que se reciba una senal de interrupcion
-    while (!senal_recibida) {
-        testear_y_mostrar_leds();//BENJA
-    }
-
-    // Finaliza los LEDs
-    finalizar_leds(pines_positivos, pines_negativos);
-
-    // Finaliza la biblioteca pigpio
-    gpioTerminate();
-
-    printf("\n");
     return 0;
 }
 
@@ -56,36 +41,36 @@ void testear_y_mostrar_leds() {//BENJA
             senal_led_coordinado(fila, columna, imagen[fila][columna]); // Mostrar la imagen
             time_sleep(0.1); // Esperar 100 milisegundos
             senal_led_coordinado(fila, columna, 0); // Apagar el LED
+            
         } //BENJA
     }
 }
 
 // --------------- CONTROLADOR ---------------
 
-// Inicializa los LEDs configurando los pines como salida
 void inicializar_leds(int pines_positivos[TAMANO], int pines_negativos[TAMANO]) {
     for (int pin = 0; pin < TAMANO; pin++) {
         gpioSetMode(pines_positivos[pin], PI_OUTPUT);
         gpioSetMode(pines_negativos[pin], PI_OUTPUT);
-        gpioWrite(pines_positivos[pin], PI_LOW);
-        gpioWrite(pines_negativos[pin], PI_HIGH);
+        control_led(pines_positivos[pin], pines_negativos[pin], 0);
     }
 }
 
-// Finaliza los LEDs configurando los pines como entrada
 void finalizar_leds(int pines_positivos[TAMANO], int pines_negativos[TAMANO]) {
     for (int pin = 0; pin < TAMANO; pin++) {
-        gpioWrite(pines_positivos[pin], PI_LOW);
-        gpioWrite(pines_negativos[pin], PI_HIGH);
+        control_led(pines_positivos[pin], pines_negativos[pin], 0);
         gpioSetMode(pines_positivos[pin], PI_INPUT);
         gpioSetMode(pines_negativos[pin], PI_INPUT);
     }
 }
 
-// Controla un LED especifico de la matriz
 void senal_led_coordinado(int fila, int columna, int estado) {
     int pin_positivo = pines_positivos[columna];
     int pin_negativo = pines_negativos[fila];
+    control_led(pin_positivo, pin_negativo, estado);
+}
+
+void control_led(int pin_positivo, int pin_negativo, int estado) {
     if (estado) {
         gpioWrite(pin_positivo, PI_HIGH);
         gpioWrite(pin_negativo, PI_LOW);
@@ -95,8 +80,28 @@ void senal_led_coordinado(int fila, int columna, int estado) {
     }
 }
 
+int inicializar_gpio() {
+    if (gpioInitialise() == PI_INIT_FAILED) {
+        return 0;
+    }
+    inicializar_leds(pines_positivos, pines_negativos);
+    return 1;
+}
+
+int finalizar_gpio() {
+    finalizar_leds(pines_positivos, pines_negativos);
+    gpioTerminate();
+}
+
 
 // --------------- MENU ---------------
+
+void menu(){
+    
+    int opcion;
+    printf("IN");
+
+}
 
 
 
