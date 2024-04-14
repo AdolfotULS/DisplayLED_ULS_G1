ef9ef7a697bb86b15a2764377fe4b851fbc49ab7
AdolfotULS <166766031+AdolfotULS@users.noreply.github.com>
2024-04-13 21:05:34 -0400

    Actualización del Proyecto: Display Imagenes
    ### Cambios en el Proyecto
    
    #### Adiciones:
    
    - Se agregaron nuevas librerías, constantes y declaraciones de funciones relacionadas con la renderización de animaciones e imágenes.
    - Se implementaron funciones para renderizar animaciones e imágenes, mostrar fotogramas y copiar imágenes.
    - Se agregaron nuevas variables para almacenar imágenes y animaciones.
    
    #### Modificaciones:
    
    - Se modificó la función `main()` para incluir un menú interactivo y se cambió su tipo de retorno.
    - Se ajustó el tiempo de espera en la función `testear_y_mostrar_leds()`.
    - Se cambió el tipo de retorno de la función `finalizar_gpio()`.
    
    #### Eliminaciones:
    
    - Se eliminó la función `menu()` original y su implementación.
    
    ### Resumen de los Cambios
    
    Se agregaron nuevas funcionalidades para la renderización de animaciones e imágenes, se implementó un menú interactivo en la función `main()`, se realizaron ajustes menores en algunas funciones existentes y se eliminó la función `menu()` original.
    
    Co-Authored-By: JereR1621 <164182331+JereR1621@users.noreply.github.com>

--- main.c
+++ main.c
@@ -3,10 +3,16 @@
 #include <signal.h>  // Biblioteca para manejar señales
 #include <stdio.h>   // Biblioteca para entrada/salida
 #include <pigpio.h>  // Biblioteca para controlar GPIO de Raspberry Pi
+//#include <unistd.h> //Testing
+//#include <time.h> //Testing
 
 // --------------- GLOBALES ---------------
 
 #define TAMANO 8 // Tamaño matriz de LEDs
+#define FOTOGRAMAS_POR_SEGUNDO 30 
+#define TIEMPO_POR_FOTOGRAMA (1.0 / FOTOGRAMAS_POR_SEGUNDO)
+#define FOTOGRAMAS_POR_SEGUNDO_ANIMACION 3 
+#define TIEMPO_POR_FOTOGRAMA_ANIMACION (1.0 / FOTOGRAMAS_POR_SEGUNDO_ANIMACION)
 
 const int pines_positivos[TAMANO] = {26, 19, 13, 6, 5, 11, 9, 10}; // Pines GPIO columnas
 
@@ -16,37 +22,195 @@ volatile sig_atomic_t senal_recibida = 0; // Almacenar la senal recibida
 
 // --------------- DECLARACIONES ---------------
 
-
+int inicializar_gpio();
+void finalizar_gpio();
+int mostrar_menu();
+void copiar_imagen();
+void extraer_frame();
+void renderizar_animacion();
+void renderizar_animacion_2();
+void renderizar_imagen();
+void renderizar_imagen_2();
+void mostrar_fotograma();
+void mostrar_fotograma_2();
+void testear_y_mostrar_leds()
+void senal_led_coordinado();
+void control_led();
+void mostrar_menu();
+void copiar_imagen();
 
 // --------------- MAIN ---------------
 
-void main() {
-    signal(SIGINT, sigint_handler);
-    if (inicializar_gpio == 0){
+void main() {  
+    if (inicializar_gpio() == 0){
         printf("No se ha podido Inicializar GPIO.\n")
         return 1;
     }
+    signal(SIGINT, sigint_handler);
 
     //Menu
+    int opcion = 0;
+    while (opcion != 3)
+    {
+        opcion = mostrar_menu();
+    }
 
+    finalizar_gpio();
+    
     return 0;
 }
 
 // --------------- DISPLAY --------------
 
-// Muestra una imagen en la matriz de LEDs y hace parpadear los LEDs
-void testear_y_mostrar_leds() {//BENJA
+//TIEMPO TOTAL DE LA IMAGEN EN PANTALLA
+
+//SUMA DE TODO EL TIEMPO QUE TOME REFRESCAR LA CANTIDAD DE FRAMES_POR_SEGUNDOS NO DEBE DE SER SUPERIOR A seg_duracion
+void renderizar_animacion(int num_repeticiones,float seg_duracion_por_imagen, int animacion[][TAMANO][TAMANO]) {
+    for (int repeticion = 0; repeticion < num_repeticiones; repeticion++) {
+        for (int i = 0; i < num_imagenes; i++) {
+            renderizar_imagen(seg_duracion_por_imagen, animacion[i]);
+        }
+    }
+}
+
+//TIEMPO
+void renderizar_animacion_3(float seg_duracion, int animcion[][TAMANO][TAMANO], int frames_animacion) {
+    int frame_actual = 0; //Frame que esta mostrando
+    int frames_animacion = frames_animacion - 1; // Cuantos frames contiene la animacion
+    double tiempo_inicio = time_time();
+
+    // Bucle que renderiza la imagen por x tiempo
+    // Tiene que mostrar los todos los frames de animacion, y volver a repetirse por el x tiempo
+    // Finalizar luego del x tiempo
+    while ((time_time() - tiempo_inicio) < seg_duracion)
+    {
+        if (frame_actual > frames_animacion) { // La animacion termino entonces se repite
+            frame_actual = 0;
+        }
+
+        int imagen_actual[TAMANO][TAMANO]; //Imagen - Frame
+        extraer_frame(frame_actual, animacion, imagen_actual); // Extraer el frame que corresponde
+        renderizar_imagen(TIEMPO_POR_FOTOGRAMA_ANIMACION, frame_actual); // Renderizar imagen por el tiempo correspondiente
+        frame_actual++;
+    }
+    
+}
+
+void renderizar_animacion_2(float seg_duracion, int animcion[][TAMANO][TAMANO], int frames_animacion) {
+    int frames_totales = seg_duracion * FOTOGRAMAS_POR_SEGUNDO_ANIMACION; //Cuantos fotogramas se pueden en ese tiempoo
+    int frame_contador = 0; //Frame actual hasta los totales
+    int frame_actual = 0; //Frame que esta mostrando
+    int frames_animacion = frames_animacion - 1; // Cuantos frames contiene la animacion
+
+    // Bucle que renderiza la imagen por x tiempo
+    // Tiene que mostrar los todos los frames de animacion, y volver a repetirse por el x tiempo
+    // Finalizar luego del x tiempo
+    while (frame_contador < frames_totales)
+    {
+        if (frame_actual > frames_animacion) { // La animacion termino entonces se repite
+            frame_actual = 0;
+        }
+
+        int imagen_actual[TAMANO][TAMANO]; //Imagen - Frame
+        extraer_frame(frame_actual, animacion, imagen_actual); // Extraer el frame que corresponde
+        renderizar_imagen(TIEMPO_POR_FOTOGRAMA_ANIMACION, frame_actual); // Renderizar imagen por el tiempo correspondiente
+        frame_actual++;
+        frame_contador++;
+    }
+    
+}
+
+void renderizar_imagen(float seg_duracion, int imagen[TAMANO][TAMANO]) {
+    int frames_totales = seg_duracion * FOTOGRAMAS_POR_SEGUNDO ; //CUanto frames en el x tiempo
+
+    int imagen_actual[TAMANO][TAMANO];
+    copiar_imagen(imagen, imagen_actual);
+    
+    int frames = 0;
+    while (frames < frames_totales)
+    {
+        mostrar_fotograma(imagen_actual);
+        frames++; 
+    }
+}
+
+//TIEMPO
+void renderizar_imagen_2(float seg_duracion, int imagen[TAMANO][TAMANO]) {
+    double tiempo_inicio = time_time();
+
+    int imagen_actual[TAMANO][TAMANO];
+    copiar_imagen(imagen, imagen_actual);
+    
+    int frames = 0;
+    while ((time_time() - tiempo_inicio) < seg_duracion) {
+        mostrar_fotograma(imagen_actual);
+        frames++;
+    }
+}
+
+
+void mostrar_fotograma(int imagen[TAMANO][TAMANO]) {
+    // Tiempo de inicio del fotograma
+    double tiempo_inicio = time_time();
+    // Calcular el tiempo de espera para cada LED
+    double tiempo_espera = TIEMPO_POR_FOTOGRAMA / (TAMANO * TAMANO);
+
+    // Mostrar el fotograma en la matriz de LEDs
     for (int columna = 0; columna < TAMANO; columna++) {
         for (int fila = 0; fila < TAMANO; fila++) {
-            senal_led_coordinado(fila, columna, imagen[fila][columna]); // Mostrar la imagen
-            time_sleep(0.1); // Esperar 100 milisegundos
-            senal_led_coordinado(fila, columna, 0); // Apagar el LED
-            
-        } //BENJA
+            int valor_pixel = imagen[TAMANO][TAMANO];
+            if (valor_pixel == 1) {
+                senal_led_coordinado(fila, columna, 1);
+                time_sleep(tiempo_espera);
+                senal_led_coordinado(fila, columna, 0);
+            } else {
+                time_sleep(tiempo_espera);
+            }
+        }
+    }
+
+    // Calcular el tiempo transcurrido
+    double tiempo_transcurrido = time_time() - tiempo_inicio;
+    
+    // Verificar si el tiempo transcurrido es menor que TIEMPO_POR_FOTOGRAMA
+    if (tiempo_transcurrido < TIEMPO_POR_FOTOGRAMA) {
+        // Esperar el tiempo restante del fotograma
+        time_sleep(TIEMPO_POR_FOTOGRAMA - tiempo_transcurrido);
+    }
+}
+
+// EN CASO DE NO FUNCIONAR EL PRIMERO
+void mostrar_fotograma_2(int imagen[TAMANO][TAMANO]) {
+    // Calcular el tiempo de espera para cada LED
+    double tiempo_espera = TIEMPO_POR_FOTOGRAMA / (TAMANO * TAMANO);
+
+    // Mostrar el fotograma en la matriz de LEDs
+    for (int columna = 0; columna < TAMANO; columna++) {
+        for (int fila = 0; fila < TAMANO; fila++) {
+            int valor_pixel = imagen[TAMANO][TAMANO];
+            if (valor_pixel == 1) {
+                senal_led_coordinado(fila, columna, 1);
+                time_sleep(tiempo_espera);
+                senal_led_coordinado(fila, columna, 0);
+            } else {
+                time_sleep(tiempo_espera);
+            }
+        }
     }
 }
 
 // --------------- CONTROLADOR ---------------
+
+void testear_y_mostrar_leds() {
+    for (int columna = 0; columna < TAMANO; columna++) {
+        for (int fila = 0; fila < TAMANO; fila++) {
+            senal_led_coordinado(fila, columna, imagen[fila][columna]); // Mostrar la imagen
+            time_sleep(0.0001); //Segundos
+            senal_led_coordinado(fila, columna, 0); // Apagar el LED
+            
+        } 
+    }
+}
 
 void inicializar_leds(int pines_positivos[TAMANO], int pines_negativos[TAMANO]) {
     for (int pin = 0; pin < TAMANO; pin++) {
@@ -88,27 +252,62 @@ int inicializar_gpio() {
     return 1;
 }
 
-int finalizar_gpio() {
+void finalizar_gpio() {
     finalizar_leds(pines_positivos, pines_negativos);
     gpioTerminate();
 }
 
 
-// --------------- MENU ---------------
+// --------------- MENU(provicional) ---------------
 
-void menu(){
-    
+void mostrar_menu(){
     int opcion;
-    printf("IN");
+    printf("INGRESE UNA OPCION \n1-Imagen 2-Video 3-Salir");
+    scanf("%d",&opcion);
+    switch (opcion)
+    {
+    case 1:
+            printf("\nSeleccione la imagen \n1- Carita feliz 2- Opcion2");
+            return 1;
 
+            break;
+    case 2:
+            printf("\nSeleccione la animacion");
+            return 2
+
+            break;
+    default:
+            printf("");
+            return 3
+
+            break;
+    }
+    
+    return opcion;
 }
-
-
 
 // --------------- UTILS ---------------
 
 void sigint_handler(int signal) {
     senal_recibida = signal;
+}
+
+void copiar_imagen(int imagen_original[TAMANO][TAMANO], int imagen_copia[TAMANO][TAMANO]) {
+    int imagen_copia[TAMANO][TAMANO];
+    for (int fila = 0; fila < TAMANO; fila++) {
+        for (int columna = 0; columna < TAMANO; columna++) {
+            imagen_copia[fila][columna] = imagen_original[fila][columna];
+        }
+    }
+}
+
+void extraer_frame(int frame, int animcion[][TAMANO][TAMANO], int frame_extraido[TAMANO][TAMANO]) {
+    int frame_actual[TAMANO][TAMANO];
+    for (int i = 0; i < TAMANO; i++) {
+        for (int j = 0; j < TAMANO; j++) {
+            frame_actual[i][j] = animacion[frame][i][j];
+        }
+    }
 }
 
 // --------------- IMAGENES ---------------
@@ -123,3 +322,29 @@ int imagen[TAMANO][TAMANO] = {
         {0, 1, 0, 0, 0, 0, 1, 0},
         {1, 0, 0, 0, 0, 0, 0, 1}
 };
+int imagen2[TAMANO][TAMANO] = {
+        {1, 0, 0, 0, 0, 0, 0, 1},
+        {0, 1, 0, 0, 0, 0, 1, 0},
+        {0, 0, 1, 0, 0, 1, 0, 0},
+        {0, 0, 0, 1, 1, 0, 0, 0},
+        {0, 0, 0, 1, 1, 0, 0, 0},
+        {0, 0, 1, 0, 0, 1, 0, 0},
+        {0, 1, 0, 0, 0, 0, 1, 0},
+        {1, 0, 0, 0, 0, 0, 0, 1}
+};
+int imagen3[TAMANO][TAMANO] = {
+        {1, 0, 0, 0, 0, 0, 0, 1},
+        {0, 1, 0, 0, 0, 0, 1, 0},
+        {0, 0, 1, 0, 0, 1, 0, 0},
+        {0, 0, 0, 1, 1, 0, 0, 0},
+        {0, 0, 0, 1, 1, 0, 0, 0},
+        {0, 0, 1, 0, 0, 1, 0, 0},
+        {0, 1, 0, 0, 0, 0, 1, 0},
+        {1, 0, 0, 0, 0, 0, 0, 1}
+};
+//--------------- ANIMACION ---------------
+int animacion[][TAMANO][TAMANO] = {
+        imagen1,
+        imagen2,
+        imagen3
+    };
